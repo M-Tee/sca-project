0fec4504c28a38b188c4673ac3fdcab9b55c2ca2
mt <margarettom6@gmail.com>
2020-11-13 11:31:11 +0300

    Configure server to open a url on browser

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -6,11 +6,13 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
   }
+  open('http://localhost:' + port)
   console.log(`listening on Port ${port}`);
 });
