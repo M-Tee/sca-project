28be8e9bbe7e6f41518924c04fb2b635b26cc469
mt <margarettom6@gmail.com>
2020-11-25 11:02:24 +0300

    Eliminated path import redundancy

--- webpack.config.dev.mjs
+++ webpack.config.dev.mjs
@@ -1,6 +1,5 @@
-import path from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
