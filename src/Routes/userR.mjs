1c5568b87a852cbb7a2738f00b7bc3d59294b03b
mt <margarettom6@gmail.com>
2020-12-02 11:10:50 +0300

    Get users route

--- src/Routes/userR.mjs
+++ src/Routes/userR.mjs
@@ -1,7 +1,10 @@
 import express from 'express';
-import controller from '../Controllers/userController.mjs'
+import controller from '../Controllers/userC.mjs'
 
 const router = express.Router();
+
+router.route('/users')
+  .get(controller.getUsers);
 
 router.route('/signup')
   .post(controller.createUser);
