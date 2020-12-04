285c0732e23824cb4297c1c3ef34af5b5421efa6
mt <margarettom6@gmail.com>
2020-12-04 13:05:41 +0300

    Save token to db

--- src/Controllers/userC.mjs
+++ src/Controllers/userC.mjs
@@ -22,30 +22,28 @@ async function login(req, res) {
       if (err) {
         return res.send(err);
       };
-
+      //Check if user exits and run bcrypt compare
       if (!user) {
-        auth('23345','3455');
+        auth('23345', '3455');
         return res.status(404).send('Wrong email and Password');
       }
+      //Capture token
       const token = auth(req.body.password, user.password, user);
+      saveToken(user, token);
       console.log(token);
-
-      //attempting: save token to db
-      // user.token = token;
-      // user.save((err) => {
-      //   if (err) {
-      //     return res.send(err);
-      //   }
-      //   return res.json(user);
-      // });
-
-      return res.send(`Welcome back ${user.firstName}!`)
-      //Check if user exits.
-      //compare passwords
-      //generate token
-      //save token to db
     }
   );
+  //save token to db
+  async function saveToken(user, token) {
+    user.token = token;
+    await user.save((err) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.send(`Welcome back ${user.firstName}!`);
+      return res.json(user);
+    });
+  };
 };
 
 //fetch users Logic.
@@ -57,6 +55,5 @@ async function getUsers(req, res) {
     return res.json(users)
   });
 };
-
 
 export default { createUser, getUsers, login };
