d242d46668dd110f6b5670704387f2a1647f0deb
mt <margarettom6@gmail.com>
2020-12-05 11:47:59 +0300

    Export individual functions

--- src/Middleware/auth.mjs
+++ src/Middleware/auth.mjs
@@ -3,17 +3,15 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 //Async/await prevent generateToken from firing before bcrypt resolves
-async function passwordAuth(password1, password2, user){
-  if (await bcrypt.compare(password1, password2)){
-    return generateToken(user);
+export async function passwordAuth(password1, password2){
+  if ( await bcrypt.compare(password1, password2)){
+    return true;
   }
   //What exit message should i use?
-  return console.log("wrong password");
+  return false;
 };
 
-function generateToken(user) {
+export function generateToken(user) {
   return jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: 86400 });
-  // console.log(token);
 };
 
-export default passwordAuth;
