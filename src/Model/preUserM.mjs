7de2290ae6556c9b547278d2f9ef84cdf0cea7d6
mt <margarettom6@gmail.com>
2020-12-05 11:47:23 +0300

    refactor code to handle errors and logins

--- src/Model/preUserM.mjs
+++ src/Model/preUserM.mjs
@@ -1,14 +1,18 @@
 import bcrypt from 'bcrypt';
 
 export default (schema) => {
-  schema.pre('save', async function() {
-    //Get the password feild & hash the password
-    if(this.isModified('password')){
-      const salt = await bcrypt.genSalt();
-      const hash = await bcrypt.hash(this.password, salt);
-      return this.password = hash;
-  }
-  //catch any errors
-    throw new Error('Something Went Wrong');
+  schema.pre('save', async function () {
+    try {
+      //Get the password feild & hash the password
+      if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(this.password, salt);
+        return this.password = hash;
+      };
+      return this.password;
+    }
+    catch (err) {
+      return console.log(err);
+    };
   });
 };
