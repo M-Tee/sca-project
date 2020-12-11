93991dbecc33bfa1d248888ed2687138a402c180
mt <margarettom6@gmail.com>
2020-12-11 22:37:34 +0300

    upload files with multer

--- src/Controllers/assignment.C.mjs
+++ src/Controllers/assignment.C.mjs
@@ -1,42 +1,55 @@
 import Assignment from '../Model/assignment.M.mjs';
 
-async function upload(req, res) {
-  try {
-    if(!req.files) {
-      res.send({ status: false, message: 'No file uploaded' });
-    }
-      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-      // let file = req.files.file;
-      const assignmnt = new Assignment(req.files);
+// async function upload(req, res) {
+//   try {
+//     if(!req.files) {
+//       res.send({ status: false, message: 'No file uploaded' });
+//     }
+//       //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+//       // let file = req.files.file;
+//       const assignmnt = new Assignment(req.files);
       
-      //Use the mv() method to place the file in upload directory (i.e. "uploads")
-      // avatar.mv('./uploads/' + avatar.name);
+//       //Use the mv() method to place the file in upload directory (i.e. "uploads")
+//       // avatar.mv('./uploads/' + avatar.name);
 
-      //send response
-      // res.send({
-      //     status: true,
-      //     message: 'File is uploaded',
-      //     data: {
-      //         name: avatar.name,
-      //         mimetype: avatar.mimetype,
-      //         size: avatar.size
-      //     }
-      // });
+//       //send response
+//       // res.send({
+//       //     status: true,
+//       //     message: 'File is uploaded',
+//       //     data: {
+//       //         name: avatar.name,
+//       //         mimetype: avatar.mimetype,
+//       //         size: avatar.size
+//       //     }
+//       // });
 
-      await assignmnt.save();
-      res.send({
-            status: true,
-            message: 'File is uploaded',
-            data: assignmnt.file.name
-        });
+//       await assignmnt.save();
+//       res.send({
+//             status: true,
+//             message: 'File is uploaded',
+//             data: assignmnt.file.name
+//         });
     
   
+//   } catch (err) {
+//     res.status(500).send(err);
+//     console.log(err);
+//    };
+// };
+
+async function upload(req, res) {
+  try {
+    if (!req.files) {
+      res.send({ status: false, message: 'No file uploaded' });
+    }
+    await uploadFile(req, res);
+    res.status(200).send("Uploaded the file successfully: " + req.file.originalname);
+
   } catch (err) {
     res.status(500).send(err);
-    console.log(err);
-   };
+    console.log(`Could not upload the file: ${req.file.originalname}. ${err}`);
+  };
 };
-
 async function getfiles(req, res) {
   try {
     await Assignment.find((err, files) => {
@@ -53,7 +66,7 @@ async function getfiles(req, res) {
 }
 
 function getListFiles(req, res) {
-  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+  const directoryPath = __basedir + "/resources/uploads/";
 
   fs.readdir(directoryPath, function (err, files) {
     if (err) {
