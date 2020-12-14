21b8d07d3c854ffb6625848cd90b43e1765275f6
mt <margarettom6@gmail.com>
2020-12-14 18:23:09 +0300

    connect to mongoDB and add options

--- buildScripts/server.mjs
+++ buildScripts/server.mjs
@@ -13,6 +13,7 @@ import mongoose from 'mongoose';
 import flash from 'express-flash';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
+import morgan from 'morgan';
 
 /*eslint-disable no-console*/
 const app = express();
@@ -21,16 +22,25 @@ const compiler = webpack(config);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 // const mongoUri = process.env.URI;
 const db = process.env.DB;
-const options = { useNewUrlParser: true, useUnifiedTopology: true, ssl: false,};
-
+const options = { 
+  useNewUrlParser: true,
+   useUnifiedTopology: true, 
+   useCreateIndex: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 4500,
+}; 
+ 
 //Connecting to mongoDB, Passing the URI and Options
-await mongoose.connect(db, options, (err) => {
-  if(err){
-    return console.log(err);
-  }
-  return console.log('Connected to MongoDB');
-});
+  try {
+    await mongoose.connect(db, options);
+    console.log('Connected to MongoDB');
+  } catch(err){
+      console.log(err.reason);
+  };
 
+mongoose.connection.on('error', err => console.log(err));
+
+morgan('tiny');
 //Setup webpack
 app.use(WebpackDevMiddleware(compiler, { publicPath: config.output.publicPath }));
 
@@ -40,11 +50,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(cors);
 app.use(userRoutes);
-app.use(assignRoutes);
+// app.use(assignRoutes);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
 
 app.listen(port, (err) => {
   if (err) {
